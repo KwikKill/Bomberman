605112ce5bf52f3baca96cf849a75396fad26dd5
Paul <79766427+TheBloodMan49@users.noreply.github.com>
2024-03-15 11:45:19 +0100

    Added 2 levels and a way to select them

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,17 @@
 #include <iostream>
 #include "Game.h"
 
-int main()
+int main(char argc, char** argv)
 {
     srand(time(NULL));
     // Print a message to the console
     std::cout << "Game starting" << std::endl;
     Game game;
-    game.load(1);
+    // Level number is the first argument
+    int level_nb = 1;
+    if (argc > 1)
+        level_nb = atoi(argv[1]);
+    game.load(level_nb);
     std::cout << "Game loaded" << std::endl;
     game.run();
     std::cout << "Game finished" << std::endl;
